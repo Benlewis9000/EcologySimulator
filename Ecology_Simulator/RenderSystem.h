39c3d5712c6b0c85b1d1e86039e701cd60786166
Benlewis9000 <benjaminlewis9000@gmail.com>
2021-01-31 17:24:36 +0000

    Fixed project git structure.

--- Ecology_Simulator/RenderSystem.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Simulation.h"
-#include "Entity.h"
-#include "Component.h"
-
-void renderEntity(PhysicalComponent* entityPhys, VertexComponent* entityVert, SpriteComponent* entitySprite, Shader* const shader, TextureManager* textureManager, GLFWwindow* window);
--- Ecology_Simulator/TargettingSystem.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Simulation.h"
-
-void updateTarget(PhysicalComponent* entityPhys, LivingComponent* entityLiv, BehaviourComponent* entityBvr, PhysicalComponent* targPhys, LivingComponent* targLiv, int& closestTarget);
-float calculateTargetRotation(const PhysicalComponent* entityPhys, const PhysicalComponent* targetPhys);
